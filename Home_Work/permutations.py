8bf7ea41d9c6f545e598f3a78eee2c85d11ff456
Juggernaut <kirilllozanov012@gmail.com>
2021-11-30 10:10:25 +0100

    pushing permutations

--- Home_Work/permutations.py
+++ Home_Work/permutations.py
@@ -1,26 +1,16 @@
-def all_perms(elements):
-    if len(elements) <=1:
-        yield elements
-    else:
-        for perm in all_perms(elements[1:]):
-            for i in range(len(elements)):
-                # nb elements[0:1] works in both string and list contexts
-                yield perm[:i] + elements[0:1] + perm[i:]
 def permutations(array):
-    myarray = []
-    if len(array) <= 1:
-        return array
-    else: 
-     for i in range(len(array)):
-         for q in range(len(array)):
-             pass
-            
-array = [2,7,5] 
-for q in range(len(array)):
-   x = array[q]   
-   for i in range(len(array)):
-    array[x], array[i] = array[i], array[x]
-    print(array)
+ result = []
+ if len(array) <= 1:
+    return [array[:]]
+ for i in range(len(array)):
+    n = array.pop(0)
+    perms = permutations(array)
+    for perm in perms:
+       perm.append(n)
+    result.extend(perms)
+    array.append(n)
+ return result
 
+print(permutations(array=[1,9,3]))
 
   
